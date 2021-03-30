ff50c0232b750c2ea6857163b044c9f414597ebb
hamid544 <arhroud.hamid.dev@gmail.com>
2021-03-30 10:46:39 +0100

    php

---
+++ Prototype/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    form{
+        background-color:#DDD;
+        width:400px;
+        padding:30px;
+
+    }
+    </style>
+</head>
+<body>
+    <h1>Formulaire d'inscription</h1>
+    <form action="affichage.php" target="_blank" method="post" >
+        <label for="" >Nom:</label><br>
+        <input name="nom" type="text"><br>
+        <label for="" >Prénom:</label><br>
+        <input name="prenom" type="text"><br>
+        <label for="" >Émail:</label><br>
+        <input name="email" type="text"><br><br>
+
+        <button type="submit">Enregistrer</button>
+    </form>
+</body>
+</html>
